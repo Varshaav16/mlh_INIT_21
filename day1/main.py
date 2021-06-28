a6c58e9af2b9e413c330d276b2045b6df9d09c6c
Varshaav16 <sec19cs118@sairamtap.edu.in>
2021-06-28 04:07:29 +0000

    added check rows

--- day1/main.py
+++ day1/main.py
@@ -18,7 +18,7 @@ def display_board(board):
 def play_game(board):
   display_board(board)
   game_is_still_going = True
-  current_player = 0
+  current_player = "X"
 
   while game_is_still_going:
     handle_turn(board, current_player)
@@ -28,7 +28,7 @@ def play_game(board):
       game_is_still_going = False
       print("Game Over!!")
 
-   
+    flip_turn(current_player)
 
 def handle_turn(board, current_player):
   position = int(input("Enter the grid number: "))
@@ -38,9 +38,23 @@ def is_game_over(board):
   return is_win and is_tie
 
 def is_win(board):
-  return True
+  return check_rows(board) and check_columns(board) and check_diagonals(board)
+
+def check_rows(board, current_player):
+  
+  for i in range(1, 10, 3):
+    row = board[i] + board[i + 1] + board[i + 2] 
+    if row == current_player * 3:
+      return True
+
+  return False
+
 
 def is_tie(board):
   return False
 
-play_game(board)
+def flip_turn(current_player):
+  return "O" if current_player == "X" else "X"
+
+# play_game(board)
+print(check_rows(board, "X"))
